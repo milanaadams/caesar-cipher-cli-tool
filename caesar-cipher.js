d3018614aef22f9b7236be4a1e5c7f5aa43215c0
Natallia Adams <milanaadams@gmail.com>
2021-05-09 11:40:57 +0300

    feat: add file check

--- caesar-cipher.js
+++ caesar-cipher.js
@@ -4,18 +4,18 @@ function cipherText(text, shift, mode) {
     if (char.match(/[a-zA-Z]/i)) {
       const charCode = char.charCodeAt(0);
       if (charCode >= 65 && charCode <= 90) {
-        if (mode === 'encode') {
+        if ((mode === 'encode' && shift >= 0) || (mode === 'decode' && shift <= 0)) {
           return String.fromCharCode(((charCode + +shift - 65) % 26) + 65);
-        } else if (mode === 'decode') {
+        } else if ((mode === 'decode' && shift > 0) || (mode === 'encode' && shift < 0)) {
           return String.fromCharCode(((charCode - +shift - 65) % 26) + 65);
         } else {
           throw new Error('Action must be either \'encode\' or \'decode\'');
         }
       }
       if (charCode >= 97 && charCode <= 122) {
-        if (mode === 'encode') {
+        if ((mode === 'encode' && shift >= 0) || (mode === 'decode' && shift <= 0)) {
           return String.fromCharCode(((charCode + +shift - 97) % 26) + 97);
-        } else if (mode === 'decode') {
+        } else if ((mode === 'decode' && shift > 0) || (mode === 'encode' && shift < 0)) {
           return String.fromCharCode(((charCode - +shift - 97) % 26) + 97);
         } else {
           throw new Error('Shift must be either \'encode\' or \'decode\'');
--- transmit-data.js
+++ transmit-data.js
@@ -5,16 +5,27 @@ const cipherText = require('./caesar-cipher');
 
 async function transmitData(options) {
 
-  if ((options.input && !path.join(__dirname, options.input)) || (options.output && !path.join(__dirname, options.output))) {
-    process.stderr.write('Couldn\'t read the file\n');
-    process.exit();
+  if(options.input) {
+    try {
+      accessSync(path.join(__dirname, options.input), constants.R_OK | constants.W_OK);
+      console.log(path.join(__dirname, options.input))
+    } catch (err) {
+      console.error(`No access to ${options.input} file!`);
+      process.exit();
+    }
+  }
+
+  if(options.output) {
+    try {
+      accessSync(options.output && !path.join(__dirname, options.output), constants.R_OK | constants.W_OK);
+    } catch (err) {
+      console.error(`No access to ${options.output} file!`);
+      process.exit();
+    }
   }
 
   const readFrom = options.input ? fs.createReadStream(path.join(__dirname, options.input)) : process.stdin;
   const writeTo = options.output ? fs.createWriteStream(path.join(__dirname, options.output)) : process.stdout;
-
-  readFrom.on('error', () => { process.stderr.write('Couldn\'t read the input file\n'); process.exit();})
-  writeTo.on('error', () => { process.stderr.write('Couldn\'t write to the input file\n'); process.exit();})
 
   readFrom.setEncoding('utf8');
 
